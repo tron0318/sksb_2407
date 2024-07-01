a26f3b1f97d1aeced2f7554dc0b8e3ee2b2db2df
tron0318 <tron0318@naver.com>
2024-07-01 14:33:23 +0900

    init

---
+++ src/main/java/com/example/sksb/SksbApplication.java
@@ -0,0 +1,13 @@
+package com.example.sksb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SksbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SksbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sksb/SksbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sksb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SksbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
