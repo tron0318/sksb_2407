16f26ade4af01860c817a33933044b3e43a54a9f
tron0318 <tron0318@naver.com>
2024-07-01 15:36:00 +0900

    front App

--- src/main/java/com/example/sksb/SksbApplication.java
+++ src/main/java/com/example/sksb/SksbApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SksbApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SksbApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SksbApplication.class, args);
+    }
 
 }
